f7d6263a8fa424c60f5865a6ec38e770448a107b
mariafernandamiranda <mariafernandamiranda@localhost>
2023-02-07 11:16:14 -0600

    Tarea 2, semana 3

---
+++ src/main/java/tarea2DAW/Tarea2DawApplication.java
@@ -0,0 +1,13 @@
+package tarea2DAW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea2DawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea2DawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tarea2DAW/Tarea2DawApplicationTests.java
@@ -0,0 +1,13 @@
+package tarea2DAW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea2DawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
